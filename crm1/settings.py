b3207a1b1142b06c93acc776b40212b468e4b9ea
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2020-11-29 20:12:37 +0100

    master

--- crm1/settings.py
+++ crm1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lsc!djbww4+(cw&n^i*+*hanh(=5sy*qb^zapt%_1!n#n$#vtv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
